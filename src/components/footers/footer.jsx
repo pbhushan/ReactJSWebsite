e3d8c3491285ffd92580ced29d9e9f58b1990ebe
Puspendu <pb249@njit.edu>
2018-09-04 13:42:33 +0530

    footer axios implemented

--- src/components/footers/footer.jsx
+++ src/components/footers/footer.jsx
@@ -1,37 +1,100 @@
 import React from "react";
 import { Col, Container, Row, Footer } from "mdbreact";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 class FooterPage extends React.Component {
   state = {
-    footer: {
-      brand: {
-        companyName: "SUSCOM",
-        comapnyTagLine: "Write something attractive here",
-        path: "/"
-      },
-      otherLinks: {
-        title: "Useful Links",
-        items: [
-          { name: "contact", path: "/contact" },
-          { name: "About", path: "/about" },
-          { name: "Product", path: "/product" }
-        ]
-      },
-      details: {
-        title: "Contact",
-        address: "123 st.,city,state,country",
-        email: "recipent@suscom.in",
-        phone: "+91 xxx-xxx-xxxx"
-      },
-      copyright: {
-        text: " 2015 Copyright",
-        companyName: "SUSCOM",
-        path: "/"
-      },
-      developedBy: "Site Developed By Sidesh Info Tech System"
+    footer: {}
+  };
+
+  componentDidMount() {
+    this.getFooter();
+  }
+
+  getFooter = () => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/pbhushan/ReactJSWebsite/master/data/footer/footer.json"
+      )
+      .then(response => {
+        this.setState({ footer: response.data.footer });
+      });
+  };
+
+  getBrand = brand => {
+    if (brand && brand.path) {
+      return (
+        <Col md="3" lg="3" xl="3" className="mx-auto mt-3">
+          <h6 className="text-uppercase mb-4 font-weight-bold">
+            <NavLink to={brand.path}> {brand.companyName} </NavLink>
+          </h6>
+          <p>{brand.comapnyTagLine}</p>
+        </Col>
+      );
+    }
+    return;
+  };
+
+  getLinks = links => {
+    if (links && links.items && links.items.length > 0) {
+      return (
+        <Col md="3" lg="2" xl="2" className="mx-auto mt-3">
+          <h6 className="text-uppercase mb-2 font-weight-bold">
+            {links.title}
+          </h6>
+          {links.items.map(item => {
+            return (
+              <NavLink
+                key={item.name}
+                className="nav-item nav-link"
+                to={item.path}
+              >
+                {item.name}
+              </NavLink>
+            );
+          })}
+        </Col>
+      );
+    }
+    return;
+  };
+
+  getDetails = details => {
+    if (details) {
+      return (
+        <Col md="4" lg="3" xl="3" className="mx-auto mt-3">
+          <h6 className="text-uppercase mb-4 font-weight-bold">
+            {details.title}
+          </h6>
+          <p>
+            <i className="fa fa-home mr-3" /> {details.address}
+          </p>
+          <p>
+            <i className="fa fa-envelope mr-3" /> {details.email}
+          </p>
+          <p>
+            <i className="fa fa-phone mr-3" /> {details.phone}
+          </p>
+        </Col>
+      );
     }
   };
+
+  getCopyRight = copyright => {
+    if (copyright) {
+      return (
+        <Col md="4" lg="4">
+          <p className="text-center text-md-left mx-auto grey-text">
+            &copy;
+            {copyright.text}
+            <NavLink to={copyright.path}> {copyright.companyName} </NavLink>
+          </p>
+        </Col>
+      );
+    }
+  };
+
   render() {
     const {
       brand,
@@ -44,55 +107,15 @@ class FooterPage extends React.Component {
       <Footer color="stylish-color-dark" className="font-small pt-4 mt-4">
         <Container fluid className="text-center text-md-left">
           <Row className="text-center text-md-left mt-3 pb-3">
-            <Col md="3" lg="3" xl="3" className="mx-auto mt-3">
-              <h6 className="text-uppercase mb-4 font-weight-bold">
-                <NavLink to={brand.path}> {brand.companyName} </NavLink>
-              </h6>
-              <p>{brand.comapnyTagLine}</p>
-            </Col>
-
+            {this.getBrand(brand)}
             <hr className="w-100 clearfix d-md-none" />
-            <Col md="3" lg="2" xl="2" className="mx-auto mt-3">
-              <h6 className="text-uppercase mb-2 font-weight-bold">
-                {otherLinks.title}
-              </h6>
-              {otherLinks.items.map(item => {
-                return (
-                  <NavLink
-                    key={item.name}
-                    className="nav-item nav-link"
-                    to={item.path}
-                  >
-                    {item.name}
-                  </NavLink>
-                );
-              })}
-            </Col>
+            {this.getLinks(otherLinks)}
             <hr className="w-100 clearfix d-md-none" />
-            <Col md="4" lg="3" xl="3" className="mx-auto mt-3">
-              <h6 className="text-uppercase mb-4 font-weight-bold">
-                {details.title}
-              </h6>
-              <p>
-                <i className="fa fa-home mr-3" /> {details.address}
-              </p>
-              <p>
-                <i className="fa fa-envelope mr-3" /> {details.email}
-              </p>
-              <p>
-                <i className="fa fa-phone mr-3" /> {details.phone}
-              </p>
-            </Col>
+            {this.getDetails(details)}
           </Row>
           <hr />
-          <Row className="d-flex align-items-center">
-            <Col md="4" lg="4">
-              <p className="text-center text-md-left grey-text">
-                &copy;
-                {copyright.text}
-                <NavLink to={copyright.path}> {copyright.companyName} </NavLink>
-              </p>
-            </Col>
+          <Row>
+            {this.getCopyRight(copyright)}
             <Col md="4" lg="4">
               <p className="text-center">{developedBy}</p>
             </Col>
