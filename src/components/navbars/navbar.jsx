e8834226417513c8691892fed38d46bfc4c040f0
Puspendu <pb249@njit.edu>
2018-09-04 14:11:16 +0530

    navbar axios implemented

--- src/components/navbars/navbar.jsx
+++ src/components/navbars/navbar.jsx
@@ -1,46 +1,74 @@
 import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
+import axios from "axios";
+
 import "../../css/navbars/navbar.css";
 
 class Navbar extends Component {
   state = {
-    navbarBrand: {
-      path: "/",
-      name: "SUSCOM"
-    },
-    navbarPostion: {
-      selected: "right"
-    },
-    navbarConfig: [
-      {
-        path: "/home",
-        name: "Home"
-      },
-      {
-        path: "/product",
-        name: "Product"
-      },
-      {
-        path: "/about",
-        name: "About"
-      },
-      {
-        path: "/contact",
-        name: "Contact"
-      }
-    ]
+    navbarBrand: {},
+    navbarPostion: {},
+    navbarConfig: []
+  };
+
+  componentDidMount() {
+    this.getNavbar();
+  }
+
+  getNavbar = () => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/pbhushan/ReactJSWebsite/master/data/navbar/navbar.json"
+      )
+      .then(response => {
+        this.setState({
+          navbarBrand: response.data.navbarBrand,
+          navbarPostion: response.data.navbarPostion,
+          navbarConfig: response.data.navbarConfig
+        });
+      });
+  };
+
+  getNavbarBrand = brand => {
+    if (brand && brand.path) {
+      return (
+        <Link className="navbar-brand" to={brand.path}>
+          <strong>{brand.name}</strong>
+        </Link>
+      );
+    }
+  };
+  getNavItems = navItem => {
+    if (navItem.path) {
+      return (
+        <NavLink
+          key={navItem.name}
+          className="nav-item nav-link"
+          to={navItem.path}
+        >
+          {navItem.name}
+        </NavLink>
+      );
+    }
+  };
+
+  getNavs = navs => {
+    if (navs && navs.length > 0) {
+      return navs.map(nav => {
+        return this.getNavItems(nav);
+      });
+    }
   };
 
   render() {
+    const { navbarBrand, navbarPostion, navbarConfig } = this.state;
+
     const positionClasses =
-      this.state.navbarPostion.selected === "left"
-        ? "navbar-nav"
-        : "navbar-nav ml-auto";
+      navbarPostion.selected === "left" ? "navbar-nav" : "navbar-nav ml-auto";
+
     return (
       <nav className="navbar fixed-top navbar-expand-lg blue darken-4 navbar-dark">
-        <Link className="navbar-brand" to={this.state.navbarBrand.path}>
-          <strong>{this.state.navbarBrand.name}</strong>
-        </Link>
+        {this.getNavbarBrand(navbarBrand)}
         <button
           className="navbar-toggler"
           type="button"
@@ -53,19 +81,7 @@ class Navbar extends Component {
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className={positionClasses}>
-            {this.state.navbarConfig.map(nav => {
-              return (
-                <NavLink
-                  key={nav.name}
-                  className="nav-item nav-link"
-                  to={nav.path}
-                >
-                  {nav.name}
-                </NavLink>
-              );
-            })}
-          </div>
+          <div className={positionClasses}>{this.getNavs(navbarConfig)}</div>
         </div>
       </nav>
     );
